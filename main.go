0690525f949e17de457f14cbca2cd541673df7f6
Lia Brüggemann <brueggemann.l@proton.me>
2023-09-01 23:12:56 +0200

    Add Red Print

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package Lestrade
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 func LogInfo(message string) {
-  log.Printf("INFO - %v", message)
+	log.Printf("INFO - %v", message)
 }
 
 func LogWarning(message string) {
-  log.Printf("WARN - %v", message)
+	log.Printf("WARN - %v", message)
 }
 
 func LogError(message string) {
-  log.Printf("ERROR - %v", message)
+	log.Printf("ERROR - %v", message)
+}
+
+func PrintRed(message string) {
+	fmt.Println("\033[1;31m" + message + "\033[0m")
 }
