2de083793841be8de2ec84b61006ad931fc40db3
Lia Brüggemann <118379719+BlyDoesCoding@users.noreply.github.com>
2023-09-09 15:24:43 +0200

    Update main.go
    More colors

--- main.go
+++ main.go
@@ -16,7 +16,42 @@ func LogWarning(message string) {
 func LogError(message string) {
 	log.Printf("ERROR - %v", message)
 }
-
+// PrintRed prints text in red color.
 func PrintRed(message string) {
 	fmt.Println("\033[1;31m" + message + "\033[0m")
+}
+
+// PrintOrange prints text in orange color.
+func PrintOrange(message string) {
+	fmt.Println("\033[1;38;5;208m" + message + "\033[0m")
+}
+
+// PrintYellow prints text in yellow color.
+func PrintYellow(message string) {
+	fmt.Println("\033[1;33m" + message + "\033[0m")
+}
+
+// PrintGreen prints text in green color.
+func PrintGreen(message string) {
+	fmt.Println("\033[1;32m" + message + "\033[0m")
+}
+
+// PrintBlue prints text in blue color.
+func PrintBlue(message string) {
+	fmt.Println("\033[1;34m" + message + "\033[0m")
+}
+
+// PrintPurple prints text in purple color.
+func PrintPurple(message string) {
+	fmt.Println("\033[1;35m" + message + "\033[0m")
+}
+
+// PrintRainbow prints text in a rainbow of colors.
+func PrintRainbow(message string) {
+	PrintRed(message)
+	PrintOrange(message)
+	PrintYellow(message)
+	PrintGreen(message)
+	PrintBlue(message)
+	PrintPurple(message)
 }
